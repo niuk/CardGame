7c08761850335f96de5504d096e9283ab123e2c3
Karl Niu <kangyuan.niu@gmail.com>
2021-01-07 16:14:49 -0800

    reinitialize card positions on window resize

--- src/client/game.ts
+++ src/client/game.ts
@@ -3,23 +3,12 @@ import * as State from './state';
 import * as VP from './view-params';
 import * as CardImages from './card-images';
 import * as Render from './render';
-import Sprite from './sprite';
 
 // refreshing should rejoin the same game
 window.history.pushState(undefined, State.gameId, `/game?gameId=${State.gameId}&playerName=${State.playerName}`);
 
-window.onresize = VP.recalculateParameters;
-window.onscroll = VP.recalculateParameters;
-
-(<any>window).game = async function game() {
-    const joinPromise = State.joinGame(State.gameId, State.playerName);
-    await CardImages.load(); // concurrently
-    await joinPromise;
-    
+async function init() {
     VP.recalculateParameters();
-
-    // rendering must be synchronous, or else it flickers
-    window.requestAnimationFrame(Render.render);
 
     // initialize input
     while (State.gameState === undefined) {
@@ -32,4 +21,19 @@ window.onscroll = VP.recalculateParameters;
     } finally {
         unlock();
     }
+}
+
+window.onresize = init;
+
+window.onscroll = init;
+
+(<any>window).game = async function game() {
+    const joinPromise = State.joinGame(State.gameId, State.playerName);
+    await CardImages.load(); // concurrently
+    await joinPromise;
+    
+    // rendering must be synchronous, or else it flickers
+    window.requestAnimationFrame(Render.render);
+
+    await init();
 };
