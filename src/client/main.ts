e9042edf1e3c0c4ffef1a21a68a76ca6d9e2ee0b
Karl Niu <kangyuan.niu@gmail.com>
2021-01-24 21:46:49 -0800

    finish labels

--- src/client/main.ts
+++ src/client/main.ts
@@ -81,6 +81,8 @@ window.onload = async () => {
     Sprite.app.ticker.add(renderOtherPlayer);
 }
 
+const deckLabels: (PIXI.Text | undefined)[] = [];
+
 const deckDealDuration = 1000;
 let deckDealTime: number | undefined = undefined;
 function renderDeck(deltaTime: number) {
@@ -93,7 +95,7 @@ function renderDeck(deltaTime: number) {
 
     for (let i = 0; i < State.deckSprites.length; ++i) {
         const deckSprite = State.deckSprites[i];
-        if (deckSprite === undefined) throw new Error();
+        if (!deckSprite) throw new Error();
 
         if (i === deckCount - 1 &&
             Input.action !== "None" &&
@@ -117,17 +119,27 @@ function renderDeck(deltaTime: number) {
 
         deckSprite.animate(deltaTime);
     }
+    
+    let [i, y] = 上下(deckLabels, State.deckContainer, 0,
+        Sprite.app.view.width / 2 + Sprite.width / 2 + (1 + deckCount / 2) * Sprite.deckGap,
+        Sprite.app.view.height / 2 - Sprite.height / 2,
+        `︵${数(deckCount)}︶`, 13);
+
+    for (; i < deckLabels.length; ++i) {
+        // workaround for bug
+        if (deckLabels[i]?.texture) deckLabels[i]?.destroy();
+    }
 }
 
-let playerLine: PIXI.Graphics | undefined;
+let playerLines: (PIXI.Graphics | undefined)[] = [];
 let playerLabels: (PIXI.Text | undefined)[] = [];
 
 function renderPlayer(deltaTime: number) {
     const gameState = Client.gameState;
-    if (gameState === undefined) return;
+    if (!gameState) return;
 
     const sprites = State.faceSpritesForPlayer[gameState.playerIndex];
-    if (sprites === undefined) throw new Error();
+    if (!sprites) throw new Error();
 
     let i = 0;
     for (const sprite of sprites) {
@@ -141,40 +153,90 @@ function renderPlayer(deltaTime: number) {
     const container = State.playerContainers[gameState.playerIndex];
     if (!container) throw new Error();
 
-    if (!playerLine) {
-        playerLine = container.addChild(new PIXI.Graphics());
-    }
-
     const centerX = Sprite.app.view.width / 2;
     const centerY = Sprite.app.view.height - Sprite.height - Sprite.gap;
-    
-    playerLine.clear();
-    playerLine.lineStyle(0.05 * Sprite.pixelsPerCM, 0xffffff, 0xff);
-    playerLine.moveTo(centerX, Sprite.app.view.height - Sprite.height * 2 - Sprite.gap * 2);
-    playerLine.lineTo(centerX, Sprite.app.view.height);
-    playerLine.moveTo(Math.max(0, centerX - Sprite.app.view.height / 2) + Sprite.height + Sprite.gap, centerY);
-    playerLine.lineTo(Math.min(centerX + Sprite.app.view.height / 2, Sprite.app.view.width) - Sprite.height - Sprite.gap, centerY);
-
-    const addLabel = (i: number, anchorX: number, anchorY: number, positionX: number, positionY: number, s: string) => {
-        let label = playerLabels[i];
-        if (!label) {
-            label = container.addChild(new PIXI.Text(s, { fill: 'white' }));
-            playerLabels[i] = label;
-        }
-        
-        label.text = s;
-        label.anchor.set(anchorX, anchorY);
-        label.position.set(positionX, positionY);
-    };
 
     const playerState = gameState.playerStates[gameState.playerIndex];
     if (!playerState) throw new Error();
 
+    addAllLines(playerLines, container, centerX, centerY);
+    addAllLabels(playerLabels, container, centerX, centerY, playerState, true);
+}
+
+function addLine(
+    lines: (PIXI.Graphics | undefined)[],
+    container: PIXI.Container,
+    i: number,
+    moveX: number, moveY: number,
+    lineX: number, lineY: number
+) {
+    let line = lines[i];
+    if (!line) {
+        line = container.addChild(new PIXI.Graphics());
+        lines[i] = line;
+    }
+
+    line.clear();
+    line.lineStyle(0.05 * Sprite.pixelsPerCM, 0xffffff, 0xff);
+    line.moveTo(moveX, moveY);
+    line.lineTo(moveX + lineX, moveY + lineY);
+}
+
+function addAllLines(
+    lines: (PIXI.Graphics | undefined)[],
+    container: PIXI.Container,
+    centerX: number,
+    centerY: number
+) {
+    const horizontal = container.transform.rotation === 0 ?
+        Sprite.app.view.width / 2 - Sprite.height - Sprite.gap :
+        Sprite.app.view.height / 2 - Sprite.height - Sprite.gap;
+    addLine(lines, container, 0, centerX, centerY, horizontal, 0);
+    addLine(lines, container, 1, centerX, centerY, -horizontal, 0);
+    addLine(lines, container, 2, centerX, centerY, 0, Sprite.height + Sprite.gap);
+    addLine(lines, container, 3, centerX, centerY, 0, -Sprite.height - Sprite.gap);
+}
+
+function addLabel(
+    labels: (PIXI.Text | undefined)[],
+    container: PIXI.Container,
+    i: number,
+    positionX: number, positionY: number,
+    s: string,
+    fontSize?: number
+) {
+    let label = labels[i];
+    if (!label || !label.transform || !label.texture) {
+        label = container.addChild(new PIXI.Text(s, { fill: 'white' }));
+        labels[i] = label;
+    }
+    
+    label.text = s;
+    // workaround for bug
+    if (label.transform) label.position.set(positionX, positionY);
+    if (label.style) {
+        if (fontSize) {
+            label.style.fontSize = fontSize;
+        } else {
+            label.style.fontSize = 26;
+        }
+    }
+}
+
+function addAllLabels(
+    labels: (PIXI.Text | undefined)[],
+    container: PIXI.Container,
+    centerX: number,
+    centerY: number,
+    playerState: Lib.PlayerState,
+    playerIsYou: boolean
+) {
     let shareCount = playerState.shareCount;
     let revealCount = playerState.revealCount;
     let groupCount = playerState.groupCount;
     let totalCount = playerState.totalCount;
-    if (Input.action !== 'Deselect' &&
+    if (playerIsYou &&
+        Input.action !== 'Deselect' &&
         Input.action !== 'None' &&
         Input.action !== 'SortByRank' &&
         Input.action !== 'SortBySuit' &&
@@ -197,72 +259,89 @@ function renderPlayer(deltaTime: number) {
         }
     }
 
-    const 上上 = centerY - Sprite.height - Sprite.gap - 0.075 * Sprite.pixelsPerCM;
-    const 上下 = centerY - Sprite.height - Sprite.gap + 0.675 * Sprite.pixelsPerCM;
-
-    const topLeftShift = centerX - (shareCount > 0 ? Sprite.width + shareCount * Sprite.gap : Sprite.gap);
-    addLabel(0, 1, 0, topLeftShift, 上上, '得');
-    addLabel(1, 1, 0, topLeftShift, 上下, '分');
-
-    const topRightCount = revealCount - shareCount;
-    const topRightShift = centerX + (topRightCount > 0 ? Sprite.width + topRightCount * Sprite.gap : Sprite.gap);
-    addLabel(2, 0, 0, topRightShift, 上上, '出');
-    addLabel(3, 0, 0, topRightShift, 上下, '牌');
-
-    const 下上 = centerY + Sprite.gap - 0.125 * Sprite.pixelsPerCM;
-    const 下下 = centerY + Sprite.gap + 0.625 * Sprite.pixelsPerCM;
-
-    const bottomLeftCount = groupCount - revealCount;
-    const bottomLeftShift = centerX - (bottomLeftCount > 0 ? Sprite.width + bottomLeftCount * Sprite.gap : Sprite.gap);
-    addLabel(4, 1, 0, bottomLeftShift, 下上, '底');
-    addLabel(5, 1, 0, bottomLeftShift, 下下, '牌');
+    const topY = centerY - Sprite.height - Sprite.gap - 0.075 * Sprite.pixelsPerCM;
+    const topLeftCount = playerIsYou ? shareCount : totalCount - groupCount;
+    const topLeftX = centerX - (topLeftCount > 0 ? Sprite.width + topLeftCount * Sprite.gap + Sprite.deckGap : Sprite.gap) - 0.75 * Sprite.pixelsPerCM;
+    let i = 0, y;
+    [i, y] = 上下(labels, container, i, topLeftX, topY, playerIsYou ? '得分' : '持牌', 26);
     
-    const bottomRightCount = totalCount - groupCount;
-    const bottomRightShift = centerX + (bottomRightCount > 0 ? Sprite.width + bottomRightCount * Sprite.gap : Sprite.gap);
-    const bottomRight數 = 數(bottomRightCount);
-    if (!bottomRight數) throw new Error();
+    const topLeftRight數 = `︵${数(playerIsYou ?
+        playerState.cards.slice(0, shareCount).map(([suit, rank]) => Math.min(10, rank)).reduce((a, b) => a + b, 0) :
+        topLeftCount
+    )}︶`;
+    [i, y] = 上下(labels, container, i, topLeftX - 0.375 * Sprite.pixelsPerCM, topY, topLeftRight數, 13);
 
-    addLabel(6, 0, 0, bottomRightShift, 下上, <string>bottomRight數[0]);
+    const topRightCount = playerIsYou ? revealCount - shareCount : groupCount - revealCount;
+    const topRightX = centerX + (topRightCount > 0 ? Sprite.width + topRightCount * Sprite.gap + Sprite.deckGap : Sprite.gap);
+    [i, y] = 上下(labels, container, i, topRightX, topY, playerIsYou ? '出牌' : '底牌', 26);
 
-    if (bottomRight數.length > 1) {
-        addLabel(7, 0, 0, bottomRightShift, 下下, <string>bottomRight數[1]);
-    } else {
-        playerLabels[7]?.destroy();
-        playerLabels[7] = undefined;
-    }
+    const bottomY = centerY + Sprite.gap - 0.125 * Sprite.pixelsPerCM;
 
-    if (bottomRight數.length > 2) {
-        addLabel(8, 0, 0, bottomRightShift, 下下 + 0.75 * Sprite.pixelsPerCM, <string>bottomRight數[2]);
-    } else {
-        playerLabels[8]?.destroy();
-        playerLabels[8] = undefined;
+    const bottomLeftCount = playerIsYou ? groupCount - revealCount : revealCount - shareCount;
+    const bottomLeftX = centerX - (bottomLeftCount > 0 ? Sprite.width + bottomLeftCount * Sprite.gap + Sprite.deckGap : Sprite.gap) - 0.75 * Sprite.pixelsPerCM;
+    [i, y] = 上下(labels, container, i, bottomLeftX, bottomY, playerIsYou ? '底牌' : '出牌', 26);
+    
+    const bottomRightCount = playerIsYou ? totalCount - groupCount : shareCount;
+    const bottomRightX = centerX + (bottomRightCount > 0 ? Sprite.width + bottomRightCount * Sprite.gap + Sprite.deckGap : Sprite.gap);
+    [i, y] = 上下(labels, container, i, bottomRightX, bottomY, playerIsYou ? '持牌' : '得分', 26);
+
+    const bottomRight數 = `︵${数(playerIsYou ?
+        bottomRightCount :
+        playerState.cards.slice(0, shareCount).map(([suit, rank]) => rank).reduce((a, b) => a + b, 0)
+    )}︶`;
+    [i, y] = 上下(labels, container, i, bottomRightX + 0.75 * Sprite.pixelsPerCM, bottomY, bottomRight數, 13);
+
+    for (; i < labels.length; ++i) {
+        // workaround for bug
+        if (labels[i]?.texture) labels[i]?.destroy();
     }
 }
 
+function 上下(
+    labels: (PIXI.Text | undefined)[],
+    container: PIXI.Container,
+    i: number,
+    x: number,
+    y: number,
+    詞: string,
+    fontSize: number
+): [number, number] {
+    for (const 字 of 詞) {
+        addLabel(labels, container, i, x, y, 字, fontSize);
+        ++i;
+        y += fontSize * 0.03 * Sprite.pixelsPerCM;
+    }
+
+    return [i, y];
+}
+
 const digits = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十'];
-function 數(n: number) {
+function 数(n: number): string {
     if (n <= 10) {
-        return digits[n];
+        const digit = digits[n];
+        if (!digit) throw new Error();
+        return digit;
     } else if (n <= 19) {
         return `十${digits[n % 10]}`;
     } else if (n <= 99) {
         const m = n % 10;
         return `${digits[Math.floor(n / 10)]}十${m > 0 ? digits[m] : ''}`;
-    } else if (n === 100) {
-        return '一百';
+    } else if (n % 100 === 0) {
+        return `${digits[Math.floor(n / 100)]}百`;
     } else {
-        n -= 100;
+        const c = Math.floor(n / 100);
+        n = n % 100;
         if (n <= 9) {
-            return `一百零${digits[n]}`;
+            return `${digits[c]}百零${digits[n]}`;
         } else {
             const m = n % 10;
-            return `一百${digits[Math.floor(m / 10)]}十${m > 0 ? digits[m] : ''}`;
+            return `${digits[c]}百${digits[Math.floor(n / 10)]}十${m > 0 ? digits[m] : ''}`;
         }
     }
 }
 
-let otherPlayerLines: PIXI.Graphics[] = [];
-let otherPlayerLabels: PIXI.Text[][] = [];
+let otherPlayerLines: (PIXI.Graphics | undefined)[][] = [];
+let otherPlayerLabels: (PIXI.Text | undefined)[][] = [];
 
 function renderOtherPlayer(deltaTime: number) {
     const gameState = Client.gameState;
@@ -277,17 +356,20 @@ function renderOtherPlayer(deltaTime: number) {
         const faceSprites = State.faceSpritesForPlayer[playerIndex];
         if (!faceSprites) throw new Error();
 
+        const centerX = Sprite.app.view.width / 2;
+        const centerY = Sprite.height + Sprite.gap;
+
         let j = 0;
         for (const faceSprite of faceSprites) {
             if (j < playerState.shareCount) {
                 faceSprite.target = {
-                    x: Sprite.app.view.width / 2 + (playerState.shareCount - j) * Sprite.gap,
-                    y: Sprite.height + 2 * Sprite.gap
+                    x: centerX + (playerState.shareCount - j) * Sprite.gap,
+                    y: centerY + Sprite.gap
                 };
             } else {
                 faceSprite.target = {
-                    x: Sprite.app.view.width / 2 - Sprite.width - (1 + j - playerState.shareCount) * Sprite.gap,
-                    y: Sprite.height + 2 * Sprite.gap
+                    x: centerX - Sprite.width - (1 + j - playerState.shareCount) * Sprite.gap,
+                    y: centerY + Sprite.gap
                 };
             }
 
@@ -305,12 +387,12 @@ function renderOtherPlayer(deltaTime: number) {
             const localGroupCount = playerState.groupCount - playerState.revealCount;
             if (j < localGroupCount) {
                 backSprite.target = {
-                    x: Sprite.app.view.width / 2 + (localGroupCount - j) * Sprite.gap,
+                    x: centerX + (localGroupCount - j) * Sprite.gap,
                     y: 0
                 };
             } else {
                 backSprite.target = {
-                    x: Sprite.app.view.width / 2 - Sprite.width - (1 + j - localGroupCount) * Sprite.gap,
+                    x: centerX - Sprite.width - (1 + j - localGroupCount) * Sprite.gap,
                     y: 0
                 };
             }
@@ -320,18 +402,24 @@ function renderOtherPlayer(deltaTime: number) {
 
             ++j;
         }
-        
-        let otherPlayerLine = otherPlayerLines[playerIndex];
-        if (!otherPlayerLine) {
-            otherPlayerLine = new PIXI.Graphics();
-            State.playerContainers[playerIndex]?.addChild(otherPlayerLine);
-            otherPlayerLines[playerIndex] = otherPlayerLine;
-        
-            otherPlayerLine.lineStyle(0.1 * Sprite.pixelsPerCM, 0xffffff, 0xff);
-            otherPlayerLine.moveTo(Sprite.app.view.width / 2, 0);
-            otherPlayerLine.lineTo(Sprite.app.view.width / 2, 2 * Sprite.height + 2 * Sprite.gap);
-            otherPlayerLine.moveTo(Sprite.height + Sprite.gap, Sprite.height + Sprite.gap);
-            otherPlayerLine.lineTo(Sprite.app.view.width - Sprite.height - Sprite.gap, Sprite.height + Sprite.gap);
+
+        const container = State.playerContainers[playerIndex];
+        if (!container) throw new Error();
+
+        let lines = otherPlayerLines[playerIndex];
+        if (!lines) {
+            lines = [];
+            otherPlayerLines[playerIndex] = lines;
         }
+
+        addAllLines(lines, container, centerX, centerY);
+
+        let labels = otherPlayerLabels[playerIndex];
+        if (!labels) {
+            labels = [];
+            otherPlayerLabels[playerIndex] = labels;
+        }
+
+        addAllLabels(labels, container, centerX, centerY, playerState, false);
     }
 }
